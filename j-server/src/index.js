c42aa42584c95713ee6c10004e0dc93196754cb1
Aimhee <maikeyjs@gmail.com>
2020-06-27 17:59:58 +0900

    server Login

--- j-server/src/index.js
+++ j-server/src/index.js
@@ -1,11 +1,36 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const Router = require('koa-router');
+const { jwtMiddleware } = require('lib/token');
 
 const app = new Koa();
 const router = new Router();
+const api = require('./api');
 
+const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(
+  (response) => {
+    console.log('Successfully connected to mongodb');
+  }
+).catch(e => {
+  console.log(e);
+})
+
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser());
+app.use(jwtMiddleware);
+router.use('/api', api.routes());
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
-  console.log('server is listening to port ' + port)
-})
+  console.log('backend server is listening to port' + port);
+});
