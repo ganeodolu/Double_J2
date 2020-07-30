d5f0546910ea2f2629a141f2d825262e0027e1a9
ganeodolu <ganeodolu@gmail.com>
2020-07-31 01:58:05 +0900

    etc: FE setting

---
+++ mission002/book-frontend/src/containers/Base/HeaderContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header, { LoginButton } from 'components/Base/Header';
+
+function HeaderContainer() {
+  return (
+    <Header>
+      <LoginButton/>
+    </Header>
+  );
+}
+
+export default HeaderContainer;
