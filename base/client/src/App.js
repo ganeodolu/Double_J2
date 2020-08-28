8f52e573daf4baa7e9dd331482140948a8cfe2e0
Aimhee <maikeyjs@gmail.com>
2020-08-28 15:17:05 +0900

    초기 로그인

--- base/client/src/App.js
+++ base/client/src/App.js
@@ -2,8 +2,32 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { Home, Auth, Searched } from 'pages';
 import HeaderContainer from 'containers/Base/HeaderContainer';
+import storage from 'lib/storage';
+import { bindActionCreators } from 'redux';
+import { useDispatch } from 'react-redux';
+import * as userActions from 'redux/modules/user';
 
-function App() {
+function App({ location }) {
+  const dispatch = useDispatch();
+  const UserActions = bindActionCreators(userActions, dispatch);
+
+  const initializeUserInfo = async () => {
+    const loggedInfo = storage.get('loggedInfo'); // 로그인 정보를 로컬스토리지에서 가져옵니다.
+    if(!loggedInfo) return; // 로그인 정보가 없다면 여기서 멈춥니다.
+
+    UserActions.setLoggedInfo(loggedInfo);
+    try {
+        await UserActions.checkStatus();
+    } catch (e) {
+        storage.remove('loggedInfo');
+        // window.location.href = '/auth/login?expired';
+        location.href = '/auth/login?expired';
+    }
+  }
+
+  useEffect(() => {
+    initializeUserInfo();
+  })
   return (
     <div>
       <HeaderContainer/>
